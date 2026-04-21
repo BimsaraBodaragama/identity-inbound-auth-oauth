34ba122841964008d0762fb616aa922f846d74ed
Pasindu Yeshan <pasinduyeshan98@gmail.com>
2026-04-21 16:17:49 +0530

    Implement batch cache clearance for authorization codes and access tokens

--- components/org.wso2.carbon.identity.oauth/src/main/java/org/wso2/carbon/identity/oauth/cache/AuthorizationGrantCache.java
+++ components/org.wso2.carbon.identity.oauth/src/main/java/org/wso2/carbon/identity/oauth/cache/AuthorizationGrantCache.java
@@ -21,6 +21,7 @@ package org.wso2.carbon.identity.oauth.cache;
 import com.nimbusds.jwt.JWT;
 import com.nimbusds.jwt.JWTParser;
 import org.apache.commons.codec.digest.DigestUtils;
+import org.apache.commons.collections4.CollectionUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -36,6 +37,7 @@ import org.wso2.carbon.identity.oauth2.util.TokenMgtUtil;
 import org.wso2.carbon.utils.CarbonUtils;
 
 import java.text.ParseException;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -332,6 +334,19 @@ public class AuthorizationGrantCache extends
         if (StringUtils.isNotBlank(id)) {
             SessionDataStore.getInstance().clearSessionData(id, AUTHORIZATION_GRANT_CACHE_NAME);
         }
+    }
+
+    /**
+     * Clears cache entries from SessionDataStore for a batch of IDs in a single database operation.
+     *
+     * @param ids List of token/code IDs to clear from the session store.
+     */
+    public void clearFromSessionStoreBatch(List<String> ids) {
+
+        if (CollectionUtils.isEmpty(ids)) {
+            return;
+        }
+        SessionDataStore.getInstance().clearSessionDataBatch(ids, AUTHORIZATION_GRANT_CACHE_NAME);
     }
 
     /**
