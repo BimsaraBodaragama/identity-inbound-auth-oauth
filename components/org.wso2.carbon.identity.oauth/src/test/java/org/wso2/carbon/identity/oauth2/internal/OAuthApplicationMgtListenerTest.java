7cf9760f427eabf82ade19cd1ae6d5cdba65b083
thumimku <thumilan.15@cse.mrt.ac.lk>
2026-04-27 16:53:52 +0530

    fix test cases

--- components/org.wso2.carbon.identity.oauth/src/test/java/org/wso2/carbon/identity/oauth2/internal/OAuthApplicationMgtListenerTest.java
+++ components/org.wso2.carbon.identity.oauth/src/test/java/org/wso2/carbon/identity/oauth2/internal/OAuthApplicationMgtListenerTest.java
@@ -49,6 +49,7 @@ import java.sql.Connection;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.lenient;
 import static org.mockito.Mockito.mockStatic;
 import static org.mockito.Mockito.when;
 import static org.mockito.MockitoAnnotations.initMocks;
@@ -123,6 +124,7 @@ public class OAuthApplicationMgtListenerTest extends TestOAuthDAOBase {
         oAuthServerConfiguration.when(OAuthServerConfiguration::getInstance).thenReturn(mockOauthServicerConfig);
         PlainTextPersistenceProcessor processor = new PlainTextPersistenceProcessor();
         when(mockOauthServicerConfig.getPersistenceProcessor()).thenReturn(processor);
+        lenient().when(mockOauthServicerConfig.getClientSecretPersistenceProcessor()).thenReturn(processor);
 
         authorizationGrantCache = mockStatic(AuthorizationGrantCache.class);
         authorizationGrantCache.when(AuthorizationGrantCache::getInstance).thenReturn(mockAuthorizationGrantCache);
